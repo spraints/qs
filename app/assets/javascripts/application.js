67cb6c89accafa731a29d7ccd862e76d17540a26
Matt Burke <spraints@gmail.com>
2014-03-06 15:43:48 -0500

    Boot the strap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery/dist/jquery
 //= require jquery_ujs
+//= require bootstrap/dist/js/bootstrap
 //= require_tree .
