f11eb1c156279169adb5510e3a348d0b7d41f97a
Matt Burke <spraints@gmail.com>
2014-12-27 10:45:04 -0500

    Lookup by email, not uid.
    UID changes during the openid -> oauth migration.

--- app/models/omniauth_result.rb
+++ app/models/omniauth_result.rb
@@ -6,8 +6,8 @@ class OmniauthResult
   # Public: The user associated with the omniauth auth hash.
   def user
     @user ||=
-      User.where(:provider => @auth.provider, :uid => @auth.uid).first_or_initialize.tap do |user|
-        user.username = @auth.info.email
+      User.where(:provider => @auth.provider, :username => @auth.info.email).first_or_initialize.tap do |user|
+        user.uid = @auth.uid
         user.enabled = false
         user.save!
       end
