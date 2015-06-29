047ecf5206a50add589cf8a59e20cfa19559fd2b
Matt Burke <spraints@gmail.com>
2015-06-29 10:50:41 -0400

    Get by_(week|month|year) working

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -1,4 +1,5 @@
 //= require 'd3/d3'
+//= require 'moment/moment'
 
 $(function() {
 
@@ -6,17 +7,55 @@ graphs = $('.graph-placeholder');
 if(graphs.length == 0)
   return;
 
-function Normal() {
-  this.transform = function(x) { console.log(x) }
+function by(period) {
+  return function(raw) {
+    var data = new DataByPeriod(period);
+    raw.forEach(function(x) { data.append(x); });
+    return data.aggregates;
+  }
 }
-var accum = new Normal();
-//if (graphs[0].classList.contains("by_month")) {
-//  accum = new ByMonth();
-//} else if (graphs[0].classList.contains("by_year")) {
-//  accum = new ByYear();
-//} else if (graphs[0].classList.contains("year_v_year")) {
-//  accum = new YearVsYear();
-//}
+
+function DataByPeriod(period) {
+  var results = this.aggregates = [];
+
+  this.append = function(point) {
+    var aggPoint = setUpPoint(point.date);
+    aggPoint.value = aggPoint.value + point.value;
+  }
+
+  var current = null;
+
+  function setUpPoint(date) {
+    date = moment(date).startOf(period);
+    if (current) {
+      dateUnix = date.unix();
+      while (current.unix < dateUnix) {
+        newPoint(current.moment.add(1, period));
+      }
+    } else {
+      newPoint(date);
+    }
+    return current.point;
+  }
+
+  function newPoint(moment) {
+    var point = {date: moment.clone().toDate(), value: 0};
+    results.push(point);
+    current = {moment: moment, unix: moment.unix(), point: point};
+  }
+}
+
+if (graphs[0].classList.contains("by_week")) {
+  transform = by("week");
+} else if (graphs[0].classList.contains("by_month")) {
+  transform = by("month");
+} else if (graphs[0].classList.contains("by_year")) {
+  transform = by("year");
+} else if (graphs[0].classList.contains("year_v_year")) {
+  transform = year_v_year;
+} else {
+  transform = function(x) { return x; };
+}
 
 var html_data_points = $('.data-point');
 var data_points = html_data_points.map(function() { return this.dataset; });
@@ -56,8 +95,7 @@ var svg = d3.select(".graph-placeholder").append("svg")
 var data = $('.data-point').map(function() {
   return {"date": parseDate(this.dataset["x"]), "value": +this.dataset["y"]};
 }).toArray().sort(function(a, b) { return d3.ascending(a.date, b.date); });
-
-data.forEach(accum.transform);
+data = transform(data);
 
 x.domain(d3.extent(data, function(d) { return d.date; }));
 y.domain(d3.extent(data, function(d) { return d.value; }));
