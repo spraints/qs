6d3f3174c2d513913499ed62fa775e2dd7444043
Matt Burke <spraints@gmail.com>
2018-02-25 13:51:28 -0500

    bin/rake rails:upgrade

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,4 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-# OpenID relies on marshalling in the session.
-Rails.application.config.action_dispatch.cookies_serializer = :marshal
+Rails.application.config.action_dispatch.cookies_serializer = :json
